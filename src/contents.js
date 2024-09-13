7fc2f91a4927131ecef12bf3519cf1aaf32e899f
TruongDinhSang <sangprotqb@gmail.com>
2024-09-13 12:41:19 +0700

    second

---
+++ src/contents.js
@@ -0,0 +1,13 @@
+import { useState, useEffect, useLayoutEffect } from 'react';
+
+function Contents() {
+
+
+    return (
+        <div style={{ 'marginLeft': '20px' }}>
+
+        </div>
+    )
+}
+
+export default Contents;

af51524493567d9f2c798a362523670a9d2e7d73
TruongDinhSang <sangprotqb@gmail.com>
2024-10-13 14:37:13 +0700

    add

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,7 +6,7 @@ function Card({ item, handleBuy, id }) {
             <div className="sale">New</div>
             <img src={item.image} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{item.name}</h5>
+                <h5 className="card-title">{item.title}</h5>
                 <p className="card-text"><span className="older-price">${item.price}.00</span>
                     {item.salePrice ? (<span className="new-price"> ${item?.salePrice}.00</span>) : <></>}
                 </p>
